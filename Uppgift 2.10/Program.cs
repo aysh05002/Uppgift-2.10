aa20ed45ec7e274a0adb91178d58bb8b43e7ead5
Admin <Admin@TEB123008>
2023-01-27 08:30:45 +0100

    Add project files.

---
+++ Uppgift 2.10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift_2_1
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hej \nJag undrar om vad dit favoritbok heter ?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Vad fint att dit favorit bok är " + bok);
+        }
+    }
+}
